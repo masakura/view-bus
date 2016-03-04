a8449bd95492bae5123be284a52dad4311e1c7c0
Tomo Masakura <tomo.masakura@gmail.com>
2016-03-05 00:45:35 +0900

    元データのロードを Promise 化

--- libs/load.js
+++ libs/load.js
@@ -7,7 +7,9 @@ function load() {
   const iconv = new Iconv('EUC-JP', 'UTF-8//TRANSLIT//IGNORE');
 
   const data = fs.readFileSync('./info.html');
-  return iconv.convert(data).toString('utf8');
+  const html = iconv.convert(data).toString('utf8');
+
+  return Promise.resolve(html);
 }
 
 module.exports = load;
--- libs/route.js
+++ libs/route.js
@@ -2,25 +2,30 @@
 
 const jsdom = require('jsdom');
 
+function get(html, number) {
+  return new Promise(resolve => {
+    jsdom.env(
+      html,
+      ['./jquery.js'],
+      (error, window) => {
+        const $routes = window.$('td[width="180"]');
+        const $route = $routes.eq(number);
+        resolve({
+          name: $route.text(),
+          information: $route.next().next().text().replace(/\n/g, ''),
+        });
+      });
+  });
+}
+
 class Route {
   constructor(loader) {
     this.loader = loader;
   }
 
   getRoute(number) {
-    return new Promise(resolve => {
-      jsdom.env(
-        this.loader(),
-        ['./jquery.js'],
-        (error, window) => {
-          const $routes = window.$('td[width="180"]');
-          const $route = $routes.eq(number);
-          resolve({
-            name: $route.text(),
-            information: $route.next().next().text().replace(/\n/g, ''),
-          });
-        });
-    });
+    return this.loader()
+      .then(html => get(html, number));
   }
 }
 
